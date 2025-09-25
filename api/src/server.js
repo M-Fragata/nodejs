acaff09a0f9b261e4f0681a8b0feee47acdaad44
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2025-09-24 21:08:15 -0300

    att

--- api/src/server.js
+++ api/src/server.js
@@ -1,7 +1,18 @@
-import http from "node:http"
+import http, { METHODS } from "node:http"
 
 const server = http.createServer((request, response) => {
-    return response.end("Meu primeiro servidor!")
+
+    const {method, url} = request
+
+    if(method === "GET" && url === "/products") {
+        return response.writeHead(200).end('Lista de produtos!')
+    } else if (method === "POST" && url === "/products") {
+        return response.writeHead(201).end('Produto cadastrado!')
+    } else {
+        return response.writeHead(404).end('Rota não encontrada!')
+    } 
+
+    
 })
 
 server.listen(3333)
