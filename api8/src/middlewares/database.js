53b59c3f11c51add18c23ee5d83b3f1ed0e8def8
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2025-12-17 18:12:17 -0300

    .

--- api8/src/middlewares/database.js
+++ api8/src/middlewares/database.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose"
 
 export const database = async () => {
-    // Exemplo de como a URI deve ficar (sem o +srv)
-const uri = "mongodb://Fragata:m4th3us1@cluster0-shard-00-00.bdixruu.mongodb.net:27017,cluster0-shard-00-01.bdixruu.mongodb.net:27017,cluster0-shard-00-02.bdixruu.mongodb.net:27017/NOME_DO_BANCO?ssl=true&replicaSet=atlas-xxxx-shard-0&authSource=admin&retryWrites=true&w=majority"
+    
+    const uri = "mongodb+srv://Fragata:m4th3us1@cluster0.bdixruu.mongodb.net/?appName=Cluster0"
 
     await mongoose.connect(uri)
     console.log("Conectado ao servidor mongoose")
